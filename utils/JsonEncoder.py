77f293f144c0e80f0fa90aa7a7072f339b0ea3cb
ColteRodriguez <111135624+ColteRodriguez@users.noreply.github.com>
2024-07-02 15:04:37 -0400

    Add files via upload

--- utils/JsonEncoder.py
+++ utils/JsonEncoder.py
@@ -46,3 +46,25 @@ def add_shape_to_json(json_string, label, points):
     updated_json_string = json.dumps(data, indent=2)
     return updated_json_string
         
+def get_shapes(img_path):
+    # Load the JSON data from a file
+    with open(img_path[:-3] + 'json') as f:
+        data = json.load(f)
+
+    # Create a dictionary to hold the labels and their corresponding points lists
+    shapes_dict = {}
+
+    # Loop through each shape in the shapes array
+    for shape in data['shapes']:
+        label = shape['label']
+        points = shape['points']
+
+        # If the label already exists in the dictionary, append the points to the list
+        if label in shapes_dict:
+            shapes_dict[label].append(points)
+        else:
+            # Otherwise, create a new entry with the label and initialize it with the points list
+            shapes_dict[label] = [points]
+
+    # Print the resulting dictionary
+    return shapes_dict
